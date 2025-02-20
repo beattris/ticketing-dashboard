f9bb59349b3551bc592e27749ded5d57548bd32e
beattris <beatriceebirim@gmail.com>
2025-02-20 05:09:07 +0100

    cleaned up the component with ngOnDestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,14 +7,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy {
   currentStatus: 'offline' | 'online' | 'unknown' = 'offline'; //this is called string literals
+  private interval?: NodeJS.Timeout;
 
   constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
-      const rnd = Math.random(); // return btw 0 - 0.999999
+    console.log('On init');
+    this.interval = setInterval(() => {
+      const rnd = Math.random(); // return btw 0 - 0.999999 
       if (rnd < 0.5) {
         this.currentStatus = 'online';
       } else if (rnd < 0.9) {
@@ -23,5 +25,13 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngAfterViewInit(){
+    console.log('AFTER VIEW INITS');
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.interval);
   }
 }
